0cc8cc4f8039c681f32d8cd5a6635082da98737f
fddddsa <fanat123123421413@mail.ru>
2024-09-05 12:54:43 +0300

    Add files via upload

---
+++ ЛЗ4/web_1 — копия/hello/hello/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from django.urls import re_path
+from firstapp import views
+urlpatterns = [
+ path('', views.index),
+ re_path(r'^about', views.about),
+ re_path(r'^contact', views.contact),
+]
